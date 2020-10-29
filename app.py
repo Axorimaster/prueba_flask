94cc79e489805390ab168af8d7d9877b6183dc0f
Axorimaster <rodrigo.zambrano@utec.edu.pe>
2020-10-29 01:33:08 -0500

    Commit 2

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -20,9 +19,57 @@ def index():
     data = cur.fetchall()
     return render_template('index.html', contactos = data)
 
-@app.route('/add_contact')
+@app.route('/add_contact', methods = ['POST'])
 def add_contact():
-    return('Contacto')
+    if request.method == 'POST':
+        nom = request.form['nombres']
+        tel = request.form['telefono']
+        email = request.form['email']
+        print('INSERT', id, nom, tel, email)
+        cur = mysql.connection.cursor()
+        cur.execute('insert into contactos(nombres,telefono,email) values(%s,%s,%s)', (nom,tel,email))
+        mysql.connection.commit()
+        flash('Contacto insertado correctamente')
+        return (redirect(url_for('index')))
+
+
+@app.route('/edit/<id>')
+def edit_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('select * from contactos where id =%s', {id})
+    data = cur.fetchall()
+    print(data[0])
+    return(render_template('edit.html', contacto=data[0]))
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('delete from contactos where id =%s', {id})
+    mysql.connection.commit()
+    flash('Contacto eliminado correctamente')
+    return (redirect(url_for('index')))
+
+@app.route('/update/<id>', methods=['POST'])
+def update_contact(id):
+    if request.method == "POST":
+        nom = request.form['nombres']
+        tel = request.form['telefono']
+        email = request.form['email']
+        print('Update', id, nom, tel, email)
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            update contactos
+            set nombres = %s,
+                telefono = %s,
+                email = %s
+            where id = %s
+        """, (nom,tel,email,id))
+        mysql.connection.commit()
+        flash('Contacto actualizado correctamente')
+        return(redirect(url_for('index')))
+
+
+
 
 if __name__ == '__main__':
     app.run(port=8000, debug = True)
